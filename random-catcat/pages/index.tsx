bb630e9663d91f22b05b62544573b80d52b30ae8
Hatano Yumi <>
2025-05-26 11:06:54 +0900

    自動デプロイテスト

--- random-catcat/pages/index.tsx
+++ random-catcat/pages/index.tsx
@@ -82,7 +82,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
 
   return (
     <div className={styles.page}>
-      <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+      <button onClick={handleClick} className={styles.button}>他のにゃんこも見る(自動デプロイテスト)</button>
       <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} />}</div>
     </div>
   );
